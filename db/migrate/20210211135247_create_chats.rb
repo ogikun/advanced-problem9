31dc0187784db6817f47ef831e6fbaa4cc53b9b4
EC2 Default User <g1621362@gmail.com>
2021-02-14 14:55:36 +0000

    [update] カラム追加

--- db/migrate/20210211135247_create_chats.rb
+++ db/migrate/20210211135247_create_chats.rb
@@ -1,7 +1,9 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
-
+      t.string :messeages
+      t.integer :user_id
+      t.integer :room_id
       t.timestamps
     end
   end
